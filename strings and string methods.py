739dae98f6ef4caa86f1778faa950e252d0a737b
Erfdrrtk <95047969+Erfdrrtk@users.noreply.github.com>
2021-12-28 14:04:41 -0600

    Add files via upload

---
+++ strings and string methods.py
@@ -0,0 +1,26 @@
+# do print() to see the results
+# these are the variables for a formatted string:
+first_name = "bob"
+last_name = "smith"
+full_name = f"Hi my name is {first_name} {last_name}"
+name_with_something = f"{full_name}. I have a boring name."
+
+# string methods:
+# len counts the amount of elements in an object including spaces
+string = '123456'
+count = (len(string))
+
+# .upper and .lower change the characters to uppercase or lowercase
+course = 'Python For Newbies'
+uppercase = (course.upper())
+lowercase = (course.lower())
+
+
+
+
+
+
+
+
+
+
