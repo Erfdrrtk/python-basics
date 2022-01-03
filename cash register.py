29070833c31390af4390d7eece825cb49c094994
Erfdrrtk <emmanuel937589@gmail.com>
2022-01-03 11:27:22 -0600

    cashier program is done

--- cash register.py
+++ cash register.py
@@ -6,19 +6,28 @@ inventory = {'grapes': 10, 'pizza': 50, 'eggs': 30, 'tomato': 25}
 basket = []
 total = []
 def cashier():
-    user_answer = input("hello what would you like to purchase? ").lower()
+    print(f"this is our inventory: {inventory}")
+    user_answer = input("what would you like to purchase? ").lower()
     while user_answer != "quit":
         if user_answer in inventory:
             basket.append(user_answer)
-            user_answer = input('anything else? ').lower()
+            user_answer = input('anything else? (type quit to end) ').lower()
         else:
-            user_answer = input('that is not in our inventory. try again: ').lower()
+            user_answer = input('''that is not in our inventory.:(type quit to end or add an item to your basket) ''')
 
 cashier()
 print(f"here are all the items in your shopping cart {basket}")
 
-answer = input("anything else. (yes or no)")
+answer = input("anything else. (yes or no) ")
 
 if answer == 'yes':
     cashier()
     print(f"here is your items {basket}")
+    for items in basket:
+        total.append(inventory[items])
+    amount_to_pay = sum(total)
+else:
+    for items in basket:
+        total.append(inventory[items])
+    amount_to_pay = sum(total)
+print(f"your total amount is: ${amount_to_pay}")
